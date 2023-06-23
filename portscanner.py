afdd6f63391fa5f22f07ff08eb8c4e530dea7566
nrfjr <90370309+nrfjr@users.noreply.github.com>
2023-06-23 11:26:58 +0800

    added data reader for open ports and useful comments

--- portscanner.py
+++ portscanner.py
@@ -1,6 +1,21 @@
 import sys
 import socket
 
+"""
+
+    scan_port - this function allows you to scan 1 address and port at a time.
+                it can be used in main function like scan_port(<address>, <port>)
+    
+    read_port_data - this function reads the data from an address, make sure that the 
+                address with corresponding port is open using scan_port for this function
+                to connect and get the available data. usage is same as scan_port.
+    
+    get_accessible_port - iterates through a range of ports from minimum port to maximum
+                (ie: from port 1 to port 65535) while using scan_port method for each iteration
+                then returns tha array of iteration.
+
+"""
+
 def scan_port(ip, port):
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,6 +30,14 @@ def scan_port(ip, port):
         sock.close()
 
     return False
+
+def read_port_data(address, port):
+    
+    s = socket.socket()
+    s.connect((address, port))
+    data = s.recv(1024) 
+    
+    return str(data)
 
 
 def get_accessible_ports(address, min_port, max_port):
@@ -36,7 +59,9 @@ def main(argv):
     max_port = int(sys.argv[3])
     ports = get_accessible_ports(address, min_port, max_port)
     for p in ports:
-         print(p)
+         if p['is_open'] == True:
+            print("IP: "+str(p["ip"])+", Port: "+str(p["port"])+", Status: "+str(p["is_open"]))
+            print("Message: " + read_port_data(p["ip"], p["port"]))
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
